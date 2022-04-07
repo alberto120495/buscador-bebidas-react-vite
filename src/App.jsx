f4a1e7100f412d8f9f2f003dc502165c1a757a0b
Alberto <albertopimentel04@gmail.com>
2022-04-07 16:46:43 -0500

    Insertando Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { Container } from "react-bootstrap";
+
 function App() {
-  return <>
-  </>;
+  return (
+    <>
+      <header className="py-5">
+        <h1>Buscador de Bebidas</h1>
+      </header>
+      <Container className="mt-5">
+        <h2>Bienvenido</h2>
+      </Container>
+    </>
+  );
 }
 
 export default App;
